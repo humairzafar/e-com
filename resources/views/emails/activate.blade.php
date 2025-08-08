bf307027da084a8c9559660f50c31b0df688887f
Muhammad-Ashfaq1 <mitf19e032@gmail.com>
2025-08-08 14:46:19 +0500

    ..

---
+++ resources/views/emails/activate.blade.php
@@ -0,0 +1,3 @@
+<p>Hello {{ $user->name }}</p>
+<p>please click on below link to active your account</p>
+<button><a href="{{ user.activate ,'$user_id' }}" style="background:green:text-color:white;text-decoration:none"></a>activate</button>
